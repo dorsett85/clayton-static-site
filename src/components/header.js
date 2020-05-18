42fca18a6e843c68761fcaa7a1155cf65cd8ad95
clayton <claytonphillipsdorsett@gmail.com>
2020-05-17 22:40:03 -0400

    Add eslint and update prettier

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,5 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import React from 'react';
 
 const Header = ({ siteTitle }) => (
   <header
@@ -18,7 +17,7 @@ const Header = ({ siteTitle }) => (
     >
       <h1 style={{ margin: 0 }}>
         <Link
-          to="/"
+          to='/'
           style={{
             color: `white`,
             textDecoration: `none`,
@@ -29,14 +28,6 @@ const Header = ({ siteTitle }) => (
       </h1>
     </div>
   </header>
-)
+);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
