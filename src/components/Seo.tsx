3842fdd2a9a6e046b5b3b3ee1ec7a32a0d697c9e
clayton <claytonphillipsdorsett@gmail.com>
2020-05-18 23:04:29 -0400

    Convert components to typescript

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,16 +1,14 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
+import { Helmet, MetaProps } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
-function SEO({ description, lang, meta, title }) {
+interface SEOProps {
+  title: string;
+  description?: string;
+  meta?: MetaProps[];
+}
+
+const SEO: React.FC<SEOProps> = ({ title, description, meta = [] }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -29,9 +27,6 @@ function SEO({ description, lang, meta, title }) {
 
   return (
     <Helmet
-      htmlAttributes={{
-        lang,
-      }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
@@ -67,22 +62,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
-      ].concat(meta)}
+        ...meta,
+      ]}
     />
   );
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-};
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 };
 
 export default SEO;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/Layout';
+import Image from '../components/Image';
+import SEO from '../components/Seo';
 
-import Layout from '../components/layout';
-import Image from '../components/image';
-import SEO from '../components/seo';
-
-const IndexPage = () => (
+const IndexPage: React.FC = () => (
   <Layout>
     <SEO title='Home' />
     <h1>Hi people</h1>
